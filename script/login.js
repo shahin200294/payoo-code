3caca9a58017b6fdc724e2c138e139c5e4ca64c9
shahin200294 <shahinsorker0483@gmail.com>
2026-02-18 18:43:20 +0600

    login page design and js setup

---
+++ script/login.js
@@ -0,0 +1,23 @@
+console.log("Login Functionality comming soon...");
+
+document.getElementById("login-btn").addEventListener("click", function() {
+    // get the mobile number input
+    const numberInput = document.getElementById("input-number");
+    const contactNumer = numberInput.value;
+    console.log(contactNumer);
+
+    // get the pin input
+    const inputPin = document.getElementById("input-pin");
+    const pin = inputPin.value;
+    console.log(pin);
+    // match pin and number
+    if(contactNumer == "012234567890" && pin == "1234"){
+        alert("login successfull");
+    }
+
+    // false login
+    else{
+        alert("login failed");
+        return;
+    }
+});
